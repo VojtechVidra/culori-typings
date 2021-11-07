2f79917331b603371ebd13283084f23794731724
Vojtěch Vidra <vojtechvidra@gmail.com>
2021-11-07 06:26:17 +0100

    feat: Add v2 typings

--- index.d.ts
+++ index.d.ts
@@ -5,3 +5,14 @@ declare module "culori" {
   export const wcagContrast;
   export const average;
 }
+
+declare module "culori/all";
+declare module "culori/css";
+
+declare module "culori/fn" {
+  export const formatHex;
+  export const displayable;
+  export const converter;
+  export const wcagContrast;
+  export const average;
+}
